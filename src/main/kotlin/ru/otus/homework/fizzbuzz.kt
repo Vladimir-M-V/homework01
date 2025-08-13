80c4f19c4b415de276372d2350605b74e0dd7057
CVB <v/mskotseba@pay-lab.ru>
2025-08-13 23:12:30 +0300

    Дамашняя работа

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,17 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    /* Функция не требует никаких дополнительных проверок. Т.к. либо исключение, либо пустой массив. */
+    val nArr = Array<Int>(n, {it})
+    val sRes = Array<String>(n, {""})
+    for ((i, v) in nArr.withIndex()) {
+        sRes[i] = if (v == 0 || ((v % 3 == 0) && (v % 5 == 0))) {
+            "FizzBuzz"
+        } else if (v % 5 == 0) {
+            "Buzz"
+        } else if (v % 3 == 0) {
+            "Fizz"
+        } else "$v"
+    }
+    return sRes
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,5 +2,13 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    val nRes = IntArray(2)
+    for ((i, v) in numbers.withIndex()) {
+        for (j in i + 1 until numbers.count()) {
+            if (v + numbers[j] == target) {
+                return intArrayOf(i, j)
+            }
+        }
+    }
+    throw IllegalArgumentException("No result")
 }
