c8394076e7bb4a41df85a0f5a3efd8efe854c2a3
CVB <v/mskotseba@pay-lab.ru>
2025-08-13 23:22:10 +0300

    Убрал не используемый код

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,7 +2,6 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    val nRes = IntArray(2)
     for ((i, v) in numbers.withIndex()) {
         for (j in i + 1 until numbers.count()) {
             if (v + numbers[j] == target) {
